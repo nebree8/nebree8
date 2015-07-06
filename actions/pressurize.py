9605032d958ca26fb641937689e0f11074f1822d
Mark Stevens <stevens.mark.a@gmail.com>
2015-07-06 00:15:54 +0000

    Added missing file.

---
+++ actions/pressurize.py
@@ -0,0 +1,16 @@
+from actions.action import Action, ActionException
+
+class HoldPressure(Action):
+  def __init__(self):
+    pass
+
+  def __call__(self, robot):
+    robot.HoldPressure()
+
+
+class ReleasePressure(Action):
+  def __init__(self):
+    pass
+
+  def __call__(self, robot):
+    robot.ReleasePressure()
