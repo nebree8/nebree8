dbcc6820dfcb6382cdafebc49c054290c547e0d5
Mark <stevens.mark.a@gmail.com>
2016-06-26 05:56:27 +0000

    Merged.

--- arduino/uc_io_bank.cc
+++ arduino/uc_io_bank.cc
@@ -38,17 +38,7 @@ void setup() {
   //arduino_io.Add(new nebree8::UCServoModule());
   arduino_io.Add(new nebree8::PressureSensorModule());
   arduino_io.Add(new nebree8::MotorModule());
-<<<<<<< HEAD
-  // I seem to run out of ram with > 100
-  // Try a teal led.
-  //led_module->LightRegion(7.0, 4.0, 0, 128, 128);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
-//pinMode(6, OUTPUT);
-//digitalWrite(6, LOW);
-=======
   arduino_io.Add(new nebree8::ServoModule());
->>>>>>> a54f6f261e8594bdc5b133985504194f8ba91104
 }
 
 void loop() {
