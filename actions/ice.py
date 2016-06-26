a236158b7de53b2b13707eacd24667b8bbdc7faa
Mark Stevens <stevens.mark.a@gmail.com>
2016-06-26 02:04:56 +0000

    Added ice dispensing module

---
+++ actions/ice.py
@@ -0,0 +1,13 @@
+from actions.action import Action, ActionException
+import time
+
+class DispenseIce(Action):
+  def __init__(self):
+    pass
+
+  def __call__(self, robot):
+    robot.ChuckVent()
+    robot.LowerCup()
+    time.sleep(3.0)
+    robot.RaiseCup()
+    time.sleep(3.0)
