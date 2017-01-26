18ac2f0d415b8eaf071abdad93854d36515c39f3
Sagar Mittal <sagar@sagarmittal.com>
2017-01-25 22:09:17 -0800

    Fix fake

--- actions/ice.py
+++ actions/ice.py
@@ -1,5 +1,4 @@
 from actions.action import Action, ActionException
-from parts import io_bank
 import time
 import threading
 
@@ -30,7 +29,5 @@ class DispenseIce(Action):
     pass
 
   def __call__(self, robot):
-    robot.io.WriteOutput(io_bank.Outputs.COMPRESSOR, 1, blocking=True)
-    robot.io.arduino.Servo(41, 0)
-    time.sleep(1.5)
-    robot.io.arduino.Servo(41, 90)
+    with robot.DispenseIce():
+      time.sleep(1.5)
