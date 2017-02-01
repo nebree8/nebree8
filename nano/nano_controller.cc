e18f1c52bfb5b86b10d5acb53a778c328350bbb4
Mark <stevens.mark.a@gmail.com>
2017-02-01 05:13:49 +0000

    Nano changes that didn't work

--- nano/nano_controller.cc
+++ nano/nano_controller.cc
@@ -6,28 +6,64 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>  // Needed by cmake to generate the pressure sensor deps. (Gross!)
 
-#include "../arduinoio/lib/uc_module.h"
 #include "../arduinoio/lib/serial_module.h"
 #include "../arduinoio/lib/arduinoio.h"
 #include "blink_module.h"
-#include "../arduino/io_delay.h"
-#include "../arduino/uc_io_module.h"
+#include "load_cell_module.h"
+#include "../HX711/HX711.h"
 
 const int SERIAL_RX_PIN = 0;
 const int SERIAL_TX_PIN = 1;
 const int LED_SIGNAL_PIN = 51;
 
+
 //SoftwareSerial *serial;
+
+//HX711 scale(DAT_PIN, CLK_PIN);
+//float calibration_factor = -7050; //-7050 worked for my 440lb max scale setup
 
 arduinoio::ArduinoIO arduino_io;
 void setup() {
   Serial.begin(115200);
+//Serial.println("HX711 calibration sketch");
+//  Serial.println("Remove all weight from scale");
+//    Serial.println("After readings begin, place known weight on scale");
+//      Serial.println("Press + or a to increase calibration factor");
+//        Serial.println("Press - or z to decrease calibration factor");
+
+//          scale.set_scale();
+//            scale.tare(); //Reset the scale to 0
+
+//              long zero_factor = scale.read_average(); //Get a baseline reading
+//                Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+//                  Serial.println(zero_factor);
   arduino_io.Add(new arduinoio::SerialRXModule(NULL, 0));
-  //arduino_io.Add(new nebree8::BlinkModule());
-  arduino_io.Add(new nebree8::IODelay());
-  arduino_io.Add(new nebree8::UCIOModule());
+  arduino_io.Add(new nebree8::BlinkModule());
+
+  const int CLK_PIN = 6;
+  const int DAT_PIN = 7;
+  arduino_io.Add(new nebree8::LoadCellModule(CLK_PIN, DAT_PIN));
 }
 
 void loop() {
   arduino_io.HandleLoopMessages();
+  
+//scale.set_scale(calibration_factor); //Adjust to this calibration factor
+
+//Serial.print("Reading: ");
+//Serial.print(scale.get_units(), 1);
+//Serial.print(" lbs"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
+//Serial.print(" calibration_factor: ");
+//Serial.print(calibration_factor);
+//Serial.println();
+
+//if(Serial.available())
+//{
+//  char temp = Serial.read();
+//  if(temp == '+' || temp == 'a')
+//    calibration_factor += 10;
+//  else if(temp == '-' || temp == 'z')
+//    calibration_factor -= 10;
+//}
+
 }
