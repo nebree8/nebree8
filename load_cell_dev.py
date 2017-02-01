82c195c130f3dc98a55810bd6662e412aaa43ab8
Mark <stevens.mark.a@gmail.com>
2017-02-01 05:52:56 +0000

    Dev code

---
+++ load_cell_dev.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import time
+
+from physical_robot import PhysicalRobot
+from parts import io_bank
+
+robot = PhysicalRobot()
+
+time.sleep(2)
+time.sleep(2000)
---
+++ move_loop.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import time
+
+from physical_robot import PhysicalRobot
+from actions.ice import PrepareIce, DispenseIce, ICE_LOCATION, StartIce, StopIce
+from actions.move import Move
+from actions.led import SetLedForValve, Led
+from parts import io_bank
+
+robot = PhysicalRobot()
+
+time.sleep(2)
+#robot.io.nano.WriteDelayRaw(13, 1, 2)
+#   robot.io.nano.Blink(13, 0.2)
+#   time.sleep(2)
+#   robot.io.nano.Blink(13, 3)
+#   ice_door = DispenseIce()
+#   ice_door(robot)
+#   time.sleep(2)
+#   robot.io.nano.Blink(13, 0.2)
+#   time.sleep(2)
+#robot.io.WriteOutput(io_bank.Outputs.COMPRESSOR, 1, blocking=True)
+robot.io.arduino.Servo(41, 90)
+
+move0 = Move(0)
+move2 = Move(-2)
+move57 = Move(-57)
+move0(robot)
+for i in range(50):
+    move2(robot)
+    move57(robot)
+    time.sleep(1)
+time.sleep(10)
+print "booting down."
